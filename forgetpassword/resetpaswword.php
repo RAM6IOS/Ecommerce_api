cdaf907ff46b4f5fa51cdb5a8419bdbeea1f7082
RAM6IOS <ios191169@gmail.com>
2024-04-24 22:20:16 +0100

    update :file resetpaswword.

--- forgetpassword/resetpaswword.php
+++ forgetpassword/resetpaswword.php
@@ -4,11 +4,11 @@ include "../functions.php";
 
 if($_SERVER["REQUEST_METHOD"] == "POST") { 
     $password = sha1($_POST['user_password']) ;
-    //$email = $_POST['user_email'];
+    $email = $_POST['user_email'];
     
     
-    $stmt = $con->prepare("UPDATE `user` SET  `user_password` = ?   ");
-    $stmt->execute(array( $password ));
+    $stmt = $con->prepare("UPDATE `user` SET  `user_password` = ?  WHERE `user_email` = ?");
+    $stmt->execute(array( $password , $email ));
     $count = $stmt->rowCount();
 
     $user = $stmt->fetch(PDO::FETCH_ASSOC);
