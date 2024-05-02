ba50ba4aec5a9ba94b49fc50fd2f0233a30a8d1c
RAM6IOS <ios191169@gmail.com>
2024-05-02 22:27:58 +0100

    creat : fil home

---
+++ home/Home.php
@@ -0,0 +1,33 @@
+<?php
+include "../Connect.php";
+include "../functions.php";
+
+
+$alldata = array();
+
+    $stmt = $con->prepare("SELECT * FROM `categories` ");
+    $count = $stmt->rowCount();
+    $stmt->execute(array());
+    $user = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    $alldata['categories'] = $user;
+        echo json_encode(['status' => 'success' ,'data' =>  $alldata]);
+        
+
+/*
+$stmt = $con->prepare("SELECT * FROM `categories` ");
+$stmt->execute(array());
+$count = $stmt->rowCount();
+
+$user = $stmt->fetchAll(PDO::FETCH_ASSOC);
+if($count > 0) {
+    echo json_encode(['status' => 'success' ,'data' =>  $user]);
+    
+
+} else {
+    echo json_encode(['status' => 'Invalid']);
+    
+}
+*/
+    
+?>
