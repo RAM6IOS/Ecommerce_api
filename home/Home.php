32a79ddcdd013dd685de98c7af971bff060ebe5b
RAM6IOS <ios191169@gmail.com>
2024-05-10 22:43:12 +0100

    update

--- home/Home.php
+++ home/Home.php
@@ -2,38 +2,14 @@
 include "../Connect.php";
 include "../functions.php";
 
-
-$alldata = array();
-
-    $stmt = $con->prepare("SELECT * FROM `categories` ");
-    $count = $stmt->rowCount();
-    $stmt->execute(array());
-    $user = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-    $stmt = $con->prepare("SELECT * FROM `items` where `items_active` != 0    ");
-    $count = $stmt->rowCount();
-    $stmt->execute(array());
-    $items = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-    $alldata['categories'] = $user;
+    $items = addItem('items', ['items_active' => 1]);
+    $categories = addItem('categories');
+    $alldata['categories'] = $categories ;
     $alldata["items"] = $items ;
-        echo json_encode(['status' => 'success' ,'data' =>  $alldata]);
+
+echo json_encode(['status' => 'success' ,'data' =>  $alldata]);
         
 
-/*
-$stmt = $con->prepare("SELECT * FROM `categories` ");
-$stmt->execute(array());
-$count = $stmt->rowCount();
 
-$user = $stmt->fetchAll(PDO::FETCH_ASSOC);
-if($count > 0) {
-    echo json_encode(['status' => 'success' ,'data' =>  $user]);
-    
-
-} else {
-    echo json_encode(['status' => 'Invalid']);
-    
-}
-*/
     
 ?>
