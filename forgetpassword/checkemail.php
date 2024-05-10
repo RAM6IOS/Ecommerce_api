67e0ad7835386c7442d7e233e25897b3d99252e2
RAM6IOS <ios191169@gmail.com>
2024-05-10 22:42:14 +0100

    update

--- forgetpassword/checkemail.php
+++ forgetpassword/checkemail.php
@@ -3,19 +3,31 @@ include "../Connect.php";
 include "../functions.php";
 
 if($_SERVER["REQUEST_METHOD"] == "POST") { 
+
     $email = $_POST['user_email'];
     $verfiycode = rand(100000, 999999);
-    $stmt = $con->prepare("SELECT * FROM `user` WHERE `user_email` = ?  ");
-    $stmt->execute(array($email));
-    $count = $stmt->rowCount();
+    $result = addItem('user', ['user_email' => $email]);
+    $count = $result["count"];
 
-    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+    /*
+    function UPDATEdata( $cloumname , array $rowname = null , array $params = null ){
+        global $con ;
 
+        $stmt = $con->prepare("UPDATE $cloumname SET  $rowname WHERE $params ");
+        $stmt->execute(array( $rowname, $params  ));
+        $count1 = $stmt->rowCount();
+
+        return $con  ;
+    };
+    */
     if($count > 0) {
         //sndEmail( $email , $verfiycode);
+       // UPDATEdata('user', ['user_verfiycode' => $verfiycode], ['user_email' => $email] );
+        
         $stmt = $con->prepare("UPDATE `user` SET  `user_verfiycode` = ?  WHERE `user_email` = ? ");
         $stmt->execute(array( $verfiycode , $email ));
         $count1 = $stmt->rowCount();
+        
 
         echo json_encode(['status' => 'success']);
         
