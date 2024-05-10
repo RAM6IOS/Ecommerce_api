9e40c11d3af936ab16d1a28114a4f9c2fed70c25
RAM6IOS <ios191169@gmail.com>
2024-05-10 22:41:48 +0100

    update

--- forgetpassword/verifycode.php
+++ forgetpassword/verifycode.php
@@ -4,20 +4,13 @@ include "../functions.php";
 
 if($_SERVER["REQUEST_METHOD"] == "POST") { 
     $verfiycode = $_POST['user_verfiycode'];
-    $stmt = $con->prepare("SELECT * FROM `user` WHERE `user_verfiycode` = ?  ");
-    $stmt->execute(array($verfiycode));
-    $count = $stmt->rowCount();
-
-    $user = $stmt->fetch(PDO::FETCH_ASSOC);
-
-    if($count > 0) {
-        //sndEmail( $email , $verfiycode);
-            echo json_encode(['status' => 'success']);  
-
-    } else {
-        echo json_encode(['status' => 'Invalid']);
-        
-    }
+$result = addItem('user', ['user_verfiycode' => $verfiycode]);
+$count = $result["count"];
+if ($count > 0) {
+    echo json_encode(['status' => 'success' ]);  
+} else {
+    echo json_encode(['status' => 'Invalid']);
+}
 }
 
     ?>
