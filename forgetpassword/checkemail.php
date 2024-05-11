59a5e2fb5d3f08e7b2bd0d415adbce5d937b35e7
RAM6IOS <ios191169@gmail.com>
2024-05-11 22:10:50 +0100

    update

--- forgetpassword/checkemail.php
+++ forgetpassword/checkemail.php
@@ -8,29 +8,9 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     $verfiycode = rand(100000, 999999);
     $result = addItem('user', ['user_email' => $email]);
     $count = $result["count"];
-
-    /*
-    function UPDATEdata( $cloumname , array $rowname = null , array $params = null ){
-        global $con ;
-
-        $stmt = $con->prepare("UPDATE $cloumname SET  $rowname WHERE $params ");
-        $stmt->execute(array( $rowname, $params  ));
-        $count1 = $stmt->rowCount();
-
-        return $con  ;
-    };
-    */
     if($count > 0) {
-        //sndEmail( $email , $verfiycode);
-       // UPDATEdata('user', ['user_verfiycode' => $verfiycode], ['user_email' => $email] );
-        
-        $stmt = $con->prepare("UPDATE `user` SET  `user_verfiycode` = ?  WHERE `user_email` = ? ");
-        $stmt->execute(array( $verfiycode , $email ));
-        $count1 = $stmt->rowCount();
-        
-
-        echo json_encode(['status' => 'success']);
-        
+        updateData2('user', ['user_verfiycode' => $verfiycode], ['user_email' => $email] );
+        echo json_encode(['status' => 'success' ]);
     } else {
         echo json_encode(['status' => 'Invalid']);
         
