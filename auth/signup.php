c1cde627bb5fdca35d2b146d079650beabc39348
RAM6IOS <ios191169@gmail.com>
2024-05-11 22:11:14 +0100

    update

--- auth/signup.php
+++ auth/signup.php
@@ -10,36 +10,21 @@ try {
         $password = sha1( $_POST['user_password']);
         $phone = $_POST["user_phone"];
         $verfiycode = rand(100000, 999999);
-        // تشفير كلمة المرور
-        //$hashed_password = password_hash($password, PASSWORD_DEFAULT);
-        $stmt = $con->prepare("SELECT * FROM `user` WHERE `user_email` = ?  OR  `user_phone` = ?");
-        $stmt->execute(array($email , $phone));
-
-        $count = $stmt->rowCount();
+        $result = addItem('user', ['user_email' => $email , 'user_phone' => $phone]);
+        $count = $result["count"];
         if ($count > 0) {
-            
             echo json_encode(['status' => 'failure']);
-            // استرجاع بيانات المستخدم المُدخل
-            //$user = $stmt->fetch(PDO::FETCH_ASSOC);
-
-           
         } else {
-                  // إعداد وتنفيذ الاستعلام
-            $stmt = $con->prepare("INSERT INTO `user`(`user_name`, `user_email`, `user_password`,`user_phone`,`user_verfiycode`) VALUES (?, ?, ? ,? ,?)");
-        // جلب عدد الصفوف المتأثرة
-
-            $stmt->execute(array($username, $email, $password,$phone ,$verfiycode));
+           instardata('user',['user_name','user_email', 'user_password','user_phone','user_verfiycode'],[ $username, $email,  $password, $phone, $verfiycode]);
             // جلب ID السجل الأخير المُدخل
             $userId = $con->lastInsertId();
-            // إعداد وتنفيذ الاستعلام
+            // إعداد وتنفيذ الاستعلا>
             $stmt = $con->prepare("SELECT * FROM `user` WHERE `user_email` = ? ");
             // Array
     $stmt->execute(array($email));
     //استرجاع بيانات المستخدم المُدخل
     $user = $stmt->fetch(PDO::FETCH_ASSOC);
-        
            // sndEmail( $email , $verfiycode);
-        
             echo json_encode(['status' => 'success', 'data' =>  $user ]);
         }
     } else {
