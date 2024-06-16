5219e7a4eab0ef886264d0365a745144b21957f7
RAM6IOS <ios191169@gmail.com>
2024-06-16 22:16:16 +0100

    update

--- home/Home.php
+++ home/Home.php
@@ -7,6 +7,18 @@ include "../functions.php";
     $alldata['categories'] = $categories ;
     $alldata["items"] = $items ;
 
+
+    $stmt = $con->prepare("SELECT * FROM `items` ");
+    $stmt->execute(array());
+    $count = $stmt->rowCount();
+    $user = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    if($count > 0) {
+        echo json_encode(['status' => 'success' ,'data' =>  $user]);
+    } else {
+        echo json_encode(['status' => 'Invalid']);
+        
+    }
+
 echo json_encode(['status' => 'success' ,'data' =>  $alldata]);
         
 
