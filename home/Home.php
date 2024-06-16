254a786d501b5328a8567996d10d618a11474e9a
RAM6IOS <ios191169@gmail.com>
2024-06-16 22:17:01 +0100

    delet

--- home/Home.php
+++ home/Home.php
@@ -8,17 +8,6 @@ include "../functions.php";
     $alldata["items"] = $items ;
 
 
-    $stmt = $con->prepare("SELECT * FROM `items` ");
-    $stmt->execute(array());
-    $count = $stmt->rowCount();
-    $user = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    if($count > 0) {
-        echo json_encode(['status' => 'success' ,'data' =>  $user]);
-    } else {
-        echo json_encode(['status' => 'Invalid']);
-        
-    }
-
 echo json_encode(['status' => 'success' ,'data' =>  $alldata]);
         
 
